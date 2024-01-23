2a1f52b9e80e2621b3abe2b76088371963977d4f
MIN <myoping99@gmail.com>
2024-01-23 11:11:42 +0900

    Update and rename chap02 to chap02_Base.java

---
+++ Java/chap02_Base.java
@@ -0,0 +1,22 @@
+
+
+	public static void main(String[] args) {
+		boolean power = true;
+		System.out.println(power);
+		int num = 50;
+		System.out.println(num);
+		System.out.println(5>2);//논리연산자는 boolean 값으로 나온다.
+		char ch1='A';
+		System.out.println(ch1);
+		String str = "Korea";//String 객체는 korea를 담을 수 있는 메모리 주소를 관리한다.
+		System.out.println(str);
+		String str2 = "안녕하세요\n자바를 배우고 있습니다.";//영문 글꼴일 때 \로 보인다.// \n 줄바꿈
+		System.out.println(str2);
+		String str3="국어\t영어\t수학";// \t 탭키 역할
+		System.out.println(str3);
+		String str4 = "철수가 말했다.\"안녕\"이라고";// \"\"따옴표 출력
+		System.out.println(str4);
+
+	}
+
+}
