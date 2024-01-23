06ec7a8fa9284d526afa3c14f49e668de0e3ecf7
MIN <myoping99@gmail.com>
2024-01-23 15:28:17 +0900

    Update chap02_연산자.java

--- Java/chap02_연산자.java
+++ Java/chap02_연산자.java
@@ -24,3 +24,29 @@
 	}
 
 }
+	public static void main(String[] args) {
+		//비교연산자(>, >=, <, <=, ==, !=) 주로 조건문과 반복문에서 사용된다.
+		int num1 = 20;
+		int num2 = 10;
+		System.out.println(num1>=num2);
+		char ch1 = 'A';
+		char ch2 = 65;
+		System.out.println(ch1==ch2);
+		
+	}
+
+
+	public static void main(String[] args) {
+		//논리연산자(&&(AND) ||(OR) !(NOT))
+		int num1 = 25;
+		int num2 = 30;
+		System.out.println(num1 >= 10 && num2 >= 10);
+		char ch1 = 'f';
+		if(ch1>=65 && ch1<=90) {//ch1>='A' && ch1<='Z'
+			System.out.println("대문자이군요");
+		}
+		if(ch1>=97 && ch1<=122) {
+			System.out.println("소문자이군요");
+		}
+		
+	}
