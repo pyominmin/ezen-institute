f7c49c42f1b1525d39d1449adcd5e56cfd3765d1
MIN <myoping99@gmail.com>
2024-01-23 18:09:23 +0900

    Update chap02_조건문.java

--- Java/chap02_조건문.java
+++ Java/chap02_조건문.java
@@ -73,3 +73,25 @@ public static void main(String[] args) {
 		}
 		
 	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("점수를 입력하세요: ");
+		int score = in.nextInt();
+
+		if (score <= 100 && score >= 0) {//중첩 if문
+			if (score >= 90 && score <= 100) {
+				System.out.println("A학점");
+			} else if (score >= 80) {
+				System.out.println("B학점");
+			} else if (score >= 70) {
+				System.out.println("C학점");
+			} else if (score >= 60) {
+				System.out.println("D학점");
+			} else {
+				System.out.println("F학점");
+			}
+		}else {
+			System.out.println("점수입력 오류!");
+		}
+	}
