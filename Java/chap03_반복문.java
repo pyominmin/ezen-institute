0849c22e04784922a2ee0ea2b2801d4f503cd59b
MIN <myoping99@gmail.com>
2024-01-24 14:15:21 +0900

    Update chap03_반복문.java

--- Java/chap03_반복문.java
+++ Java/chap03_반복문.java
@@ -66,3 +66,14 @@ public static void main(String[] args) {
 		System.out.println("1부터 100까지 짝수의 합=" + sum);
 	}
 
+
+public static void main(String[] args) {
+		for(int i = 2; i <= 9; i++) {
+			System.out.println("**" + i + "단**");
+			System.out.println();
+			for(int j = 1; j <= 9; j++) {
+				System.out.println(i +" x " + j + " = " + i*j);
+			}
+			System.out.println();
+		}
+	}
