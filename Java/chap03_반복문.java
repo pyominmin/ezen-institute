028c82bed5aebdf67c91e1ee21fff26c6211bf3b
MIN <myoping99@gmail.com>
2024-01-24 11:26:55 +0900

    Update chap03_반복문.java

--- Java/chap03_반복문.java
+++ Java/chap03_반복문.java
@@ -1,1 +1,17 @@
+반복문(for, while, do-while)
+
+  public static void main(String[] args) {
+		System.out.println("컴퓨터가 인사를 시작합니다.");
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("안녕하세요" + i);
+		}
+		System.out.println("끝~");
+	}
+
+public static void main(String[] args) {
+		System.out.println("**토큰 가격표**");
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i + "개 가격=" + i * 370);
+		}
+	}
 
