8e294fe61e1969ed4cab78ac456da7aab661dd3a
MIN <myoping99@gmail.com>
2024-01-24 14:52:09 +0900

    Update chap03_반복문.java

--- Java/chap03_반복문.java
+++ Java/chap03_반복문.java
@@ -77,3 +77,31 @@ public static void main(String[] args) {
 			System.out.println();
 		}
 	}
+
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j <= 6-i; j++) {//for(int j = i; j <= 5; j++)
+				System.out.print("*");	
+			}
+			
+			System.out.println();
+		}
+	}
+
+
+
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 15; i++) {
+			for (int b = 15; b >= i; b--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+}
