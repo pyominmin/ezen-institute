aba200dcd3c22c5e1c9cc295b92a2f190674bf71
MIN <myoping99@gmail.com>
2024-01-24 17:58:39 +0900

    Create chap03_배열.java

---
+++ Java/chap03_배열.java
@@ -0,0 +1,52 @@
+하나의 변수에 여러가지 값을 기억한다.
+
+ublic static void main(String[] args) {
+//		int score1 = 96;
+//		int score2 = 73;
+//		int score3 = 82;
+//		int sum = score1 + score2 + score3;
+//		double avg = sum / 3.0;
+		
+		int[] scores = new int[3];//하나의 변수에 여러개 값 기억
+		scores[0] = 96;
+		scores[1] = 73;
+		scores[2] = 82;
+		int sum = 0;
+		for(int i = 0; i < scores.length; i++) {
+			sum += scores[i];
+		}
+		double avg = (double)sum / scores.length;
+		
+		System.out.println("총점= " + sum);
+		System.out.println("평균= " + avg);
+
+	}
+
+
+//		int[] nums = new int[5]; //배열 선언
+//		nums[0] = 45;
+//		nums[1] = 90;
+//		nums[2] = 23;
+//		nums[3] = 42;
+//		nums[4] = 77;
+
+		int[] nums = {45, 90, 23, 42, 77}; //배열 선언과 초기화
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		System.out.println("총합=" + sum);
+	}
+
+
+int[] nums = {45, 90, 23, 42, 77, 96, 32, 43, 12, 15}; //배열 선언과 초기화
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] % 2 == 0) {
+				sum += nums[i];
+			}
+		}
+		System.out.println("총합=" + sum);
+	}
+
+
