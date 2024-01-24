ed6864bfda81182a30b8776628ee2090b6bc69d6
MIN <myoping99@gmail.com>
2024-01-24 12:28:23 +0900

    Update chap03_반복문.java

--- Java/chap03_반복문.java
+++ Java/chap03_반복문.java
@@ -15,3 +15,29 @@ public static void main(String[] args) {
 		}
 	}
 
+public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("원하는 단을 입력하세요 : ");
+		int num = in.nextInt();
+		System.out.println("**" + num + "단**");
+		for(int i = 1; i <= 9; i++) {
+			System.out.println(num + " X " + i + " = "+ num*i);
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+		int evenSum = 0;
+		int oddSum = 0;
+		for(int i = 1; i <= 100; i++) {
+			if(i % 2 == 0) {
+				evenSum += i;
+			}else{
+				oddSum += i;
+			}
+		}
+		System.out.println("1부터 100까지의 짝수의 합 = " + evenSum);
+		System.out.println("1부터 100까지의 홀수의 합 = " + oddSum);
+	}
+
