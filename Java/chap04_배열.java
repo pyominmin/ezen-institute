865cee1852897e57e4dfe9d9aa435daf7c8f0288
MIN <myoping99@gmail.com>
2024-01-25 11:01:00 +0900

    Update chap04_배열.java

--- Java/chap04_배열.java
+++ Java/chap04_배열.java
@@ -27,3 +27,33 @@
 			System.out.print(nums[i] + " ");
 		}
 	}
+
+*로또 자동번호 생성하기
+
+	public static void main(String[] args) {
+		int[] lotto = new int[6];//로또 자동번호 생성기
+		boolean run = true;
+		while (run) {
+			// 번호 6개를 무작위로 생성
+			for (int i = 0; i < lotto.length; i++) {
+				lotto[i] = (int) (Math.random() * 45) + 1;
+			}
+			run = false;
+			// 중복값 검증 알고리즘
+			
+			for (int i = 0; i < lotto.length - 1; i++) {
+				for (int j = i + 1; j < lotto.length; j++) {
+					if(lotto[i] == lotto[j]) {
+						run = true;
+						break;
+					}
+				}
+			}
+		}
+
+		System.out.println("**로또 자동번호 출력**");
+		for (int i = 0; i < lotto.length; i++) {
+			System.out.println((i + 1) + "번째 번호 : " + lotto[i]);
+		}
+	}
+
