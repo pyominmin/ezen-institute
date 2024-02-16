f48a787c5239619b58c1a182d68c4425019df8ae
MIN <myoping99@gmail.com>
2024-02-16 09:38:56 +0900

    Create SwordLaser.java

---
+++ Java/Robot만들기/인터페이스/implements/SwordLaser.java
@@ -0,0 +1,13 @@
+package com.lec.robot.v4_인터페이스.impl;
+
+import com.lec.robot.v4_인터페이스.inter.InterSword;
+
+public class SwordLaser implements InterSword{
+
+	@Override
+	public void Sword() {
+		System.out.println("광선검이 있습니다.");
+		
+	}
+
+}
