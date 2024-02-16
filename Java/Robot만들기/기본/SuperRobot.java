aa845061eb6112da31beeec4e68182b6a3ae2bd1
MIN <myoping99@gmail.com>
2024-02-16 09:26:02 +0900

    Create SuperRobot.java

---
+++ Java/Robot만들기/기본/SuperRobot.java
@@ -0,0 +1,17 @@
+package com.lec.robot.v1_기본;
+
+public class SuperRobot {
+	//변수
+	public int qty;
+			//생성자
+			public SuperRobot(int qty) {
+				this.qty = qty;
+			}
+			//메서드
+			public void shape(){System.out.println("Super로봇입니다! 팔, 다리, 머리, 몸통이 있습니다!");}
+			public void actionWalk(){System.out.println("걸을 수가 있습니다!");}
+			public void actionRun(){System.out.println("달릴 수가 있습니다!");}
+			public void actionFly(){System.out.println("날 수가 있습니다!");}
+			public void actionMissile(){System.out.println("미사일을 쏠 수 있습니다!");}
+			public void actionSword(){System.out.println("광선검이 있습니다!");}
+}
