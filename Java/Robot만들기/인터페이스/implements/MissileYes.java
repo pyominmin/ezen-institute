059edcb4caab8d7494f01c6680ed5aef62ddd4f3
MIN <myoping99@gmail.com>
2024-02-16 09:38:35 +0900

    Create MissileYes.java

---
+++ Java/Robot만들기/인터페이스/implements/MissileYes.java
@@ -0,0 +1,13 @@
+package com.lec.robot.v4_인터페이스.impl;
+
+import com.lec.robot.v4_인터페이스.inter.InterMissile;
+
+public class MissileYes implements InterMissile{
+
+	@Override
+	public void missile() {
+		System.out.println("미사일이 있습니다");
+		
+	}
+
+}
