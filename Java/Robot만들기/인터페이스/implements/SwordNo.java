4feac8c41371b3fe5f318a38b0c76f6934db4c78
MIN <myoping99@gmail.com>
2024-02-16 09:39:13 +0900

    Create SwordNo.java

---
+++ Java/Robot만들기/인터페이스/implements/SwordNo.java
@@ -0,0 +1,14 @@
+package com.lec.robot.v4_인터페이스.impl;
+
+import com.lec.robot.v4_인터페이스.inter.InterSword;
+
+public class SwordNo implements InterSword{
+
+	@Override
+	public void Sword() {
+		System.out.println("검이 없습니다.");
+		
+	}
+	
+
+}
