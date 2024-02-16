46b198caec57ef2318aeed14db926639addf915d
MIN <myoping99@gmail.com>
2024-02-16 10:24:09 +0900

    Create CheapRobot.java

---
+++ Java/Robot만들기/extnds/CheapRobot.java
@@ -0,0 +1,11 @@
+package com.lec.robot.v5_extends;
+//상속받기
+public class CheapRobot extends Robot{
+//ver5는 생성자만 있으면된다.
+	//생성자
+	public CheapRobot(String name, int qty) {
+		super(name, qty);
+		
+	}
+
+}
